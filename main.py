26c036fd8bf0eda5f811ece353d8a1cded697f50
Iljun-Kwon <jonkwon99@dongguk.edu>
2023-11-01 15:53:57 +0900

    add Major

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def result():
         result['Name']=request.form.get('name')
         result['Student Number']=request.form.get('StudentNumber')
         result['Gender']=request.form.get('Gender')
+        result['Major']=request.form.get('Major')
         
         return render_template('result.html',result=result)
 
